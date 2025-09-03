4078baabae9e5ca80c77a34cb6f3d5a2d2e3f7ff
Althea Nabila <althea.nabila@ksp.de>
2025-09-03 17:09:51 +0200

    backend Code verbessert

--- backend/src/main/java/org/dashboard/Application.java
+++ backend/src/main/java/org/dashboard/Application.java
@@ -1,15 +1,16 @@
 package org.dashboard;
 
 
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.theme.Theme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EntityScan("org.dashboard.model")
-@EnableJpaRepositories("org.dashboard.repository")
-public class Application {
+//@Theme("lumo")
+public class Application implements AppShellConfigurator {
   public static void main(String[] args) {
 
     SpringApplication.run(Application.class, args);
